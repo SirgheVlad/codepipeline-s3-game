8ff2d76af967b23c23290a2d95ebf18f8f939cd1
SirgheVlad <vlad.andrei.sirghe@gmail.com>
2025-03-03 15:43:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let i = 0; i < cardArray.length; i++) {
             const card = document.createElement('img');
             card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('width', '240px');
             card.setAttribute('data-id', i);
             card.addEventListener('click', flipCard);
             grid.appendChild(card);
